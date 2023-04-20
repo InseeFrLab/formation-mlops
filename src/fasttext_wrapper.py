8dbd0073d823aab22c07c40946aff1d02ba627f5
tseimandi <tom.seimandi@insee.fr>
2023-04-20 13:05:18 +0000

    Add src

---
+++ src/fasttext_wrapper.py
@@ -0,0 +1,79 @@
+"""
+FastText wrapper for MLflow.
+"""
+from typing import Tuple, Dict
+import fasttext
+import mlflow
+import pandas as pd
+
+from constants import TEXT_FEATURE
+from preprocessor import Preprocessor
+
+
+class FastTextWrapper(mlflow.pyfunc.PythonModel):
+    """
+    Class to wrap and use FastText models with MLflow.
+    """
+
+    def __init__(self):
+        """
+        Construct a FastTextWrapper object.
+        """
+        self.model = None
+        self.preprocessor = Preprocessor()
+
+    def load_context(self, context: mlflow.pyfunc.PythonModelContext) -> None:
+        """
+        Load the FastText model and its configuration file from an MLflow model
+        artifact. This method is called when loading an MLflow model with
+        pyfunc.load_model(), as soon as the PythonModel is constructed.
+
+        Args:
+            context (mlflow.pyfunc.PythonModelContext): MLflow context where
+                the model artifact is stored. It should contain the following
+                artifacts:
+                    - "model_path": path to the FastText model file.
+                    - "config_path": path to the configuration file.
+        """
+
+        self.model = fasttext.load_model(context.artifacts["model_path"])
+
+    def predict(
+        self, context: mlflow.pyfunc.PythonModelContext, model_input: Dict
+    ) -> Tuple:
+        """
+        Predicts the k most likely codes to a query.
+
+        Args:
+            context (mlflow.pyfunc.PythonModelContext): The MLflow model
+                context.
+            model_input (dict): A dictionary containing the input data for the
+                model. It should have the following keys:
+                - 'query': A dictionary containing the query features.
+                - 'k': An integer representing the number of predicted codes to
+                return.
+
+        Returns:
+            A tuple containing the k most likely codes to the query.
+        """
+        df = self.preprocessor.clean_text(
+            pd.DataFrame(model_input["query"]),
+            text_feature=TEXT_FEATURE
+        )
+
+        texts = df.apply(self._format_item, axis=1).to_list()
+
+        return self.model.predict(texts, k=model_input["k"])
+
+    def _format_item(self, row: pd.Series) -> str:
+        """
+        Formats a row of data into a string.
+
+        Args:
+            row (pandas.Series): A pandas series containing the row data.
+
+        Returns:
+            A formatted item string.
+        """
+        formatted_item = row[TEXT_FEATURE]
+        return formatted_item
---
+++ src/preprocessor.py
@@ -0,0 +1,61 @@
+"""
+Preprocessor.
+"""
+import pandas as pd
+import string
+from nltk.corpus import stopwords as ntlk_stopwords
+from nltk.stem.snowball import SnowballStemmer
+import unidecode
+
+
+class Preprocessor():
+    """
+    Preprocessor class.
+    """
+
+    def __init__(self):
+        """
+        Construct a Preprocessor object.
+        """
+        self.stopwords = tuple(ntlk_stopwords.words("french")) + \
+            tuple(string.ascii_lowercase)
+        self.stemmer = SnowballStemmer(language="french")
+
+    def clean_text(self, df: pd.DataFrame, text_feature: str) -> pd.DataFrame:
+        """
+        Cleans a text feature for pd.DataFrame `df` at index idx.
+
+        Args:
+            df (pd.DataFrame): DataFrame.
+            text_feature (str): Name of the text feature.
+
+        Returns:
+            df (pd.DataFrame): Clean DataFrame.
+        """
+        # Fix encoding
+        df[text_feature] = df[text_feature].map(unidecode.unidecode)
+
+        # To lowercase
+        df[text_feature] = df[text_feature].str.lower()
+
+        # Remove one-letter words
+        df[text_feature] = df[text_feature].apply(
+            lambda x: " ".join([w for w in x.split() if len(w) > 1])
+        )
+
+        # Tokenize texts
+        libs_token = [lib.split() for lib in df[text_feature].to_list()]
+
+        # Remove stopwords and stem
+        df[text_feature] = [
+            " ".join(
+                [
+                    self.stemmer.stem(word)
+                    for word in libs_token[i]
+                    if word not in self.stopwords
+                ]
+            )
+            for i in range(len(libs_token))
+        ]
+
+        return df
